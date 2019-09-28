27775b20c9491d9069bbc1afc6459f39da56dcec
Jack Wei <justrightnow@sina.com>
2019-09-28 08:37:39 +0800

    init

---
+++ src/main/java/com/wk/HelloController.java
@@ -0,0 +1,18 @@
+package com.wk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * created by wk
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String say(){
+
+        return "hello spring boot";
+    }
+}
